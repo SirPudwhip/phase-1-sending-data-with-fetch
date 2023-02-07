c0642fcd58926895e13037b8549ad96988b30b04
SirPudwhip <spamfolder3399@gmail.com>
2023-02-06 20:26:32 -0500

    It's being picky about the stream vs socket argument, but everything looks right in the DB and the data is persistent so...

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Add your code here
+function submitData () {
+    fetch('http://localhost:3000/users', userData)
+    .then(res => {
+        return res.json();
+    })
+    .then(function (object) {
+        appendObj(object);
+      })};
+
+const bodyData = {
+    name: 'Steve',
+    email: 'steve@steve.com'
+}
+
+const userData = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify(bodyData), 
+}
+
+function appendObj(object) {
+    console.log(object.id);
+    const li = document.createElement('li');
+    li.textContent = object.id;
+    document.querySelector("body").append(li);
+}
+
+submitData();
